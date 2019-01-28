8e8deedc5acbb9ee07a0b9275618ae986a404a06
sio <m40x3.1wa121@gmail.com>
2019-01-28 03:47:05 +0000

    ログアウト機能、各リンク整備、ログインユーザーのみアクセスできるよう変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
+  private
+  
+  #ログイン済ユーザーのみ許可するためのメソッド
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+  def current_user
+=begin
+  if @current_user
+    return @current_user
+  else
+    @current_user = User.find_by(id: session[:user_id])
+    return @current_user
+  end
+=end
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+=begin
+    if current_user
+      return true
+    else
+      return false
+    end
+=end
+    !!current_user
+  end
 end
