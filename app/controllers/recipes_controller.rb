cfbd2225da81a9d766492eb9aba79bb2f343fc3b
sio <m40x3.1wa121@gmail.com>
2019-01-29 08:22:16 +0000

    ランダムレシピページ作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,18 +4,19 @@ class RecipesController < ApplicationController
   
   before_action :category_new 
   def category
-    save_category
   end
   
   def random
-    save_recipe
+    @random_recipes = Recipe.order("RAND()").limit(5)
   end
   
   #最新のカテゴリ別レシピランキングを取得
   def new
+    save_category
+    save_recipe
     @recipes=[]
     @categories.each_with_index do |category,i|
-      break if i==3  #全部表示させようとすると５０４gatewaytimeoutになる。
+      break if i==12  #全部表示させようとすると５０４gatewaytimeoutになる。
       results = RakutenWebService::Recipe.ranking(category.rakuten_category_id)
       @category_id = category.rakuten_category_id
       results.each do|result|
