c4b6bef8293aeaf63ef1ca362aeaa38eec308939
sio <m40x3.1wa121@gmail.com>
2019-02-05 05:50:04 +0000

    food編集ページ追加。管理者ページ一旦完成

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,25 @@
 class FoodsController < ApplicationController
+  before_action :require_supervisor_logged_in, only: [:edit, :update]
+  
   def edit
+     @food = Food.find(params[:id])
   end
-
+  
   def update
+    @food = Food.find(params[:id])
+    if @food.update(food_params)
+      flash[:success] = 'food は正常に更新されました'
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:danger] = 'food は更新されませんでした'
+      redirect_back(fallback_location: root_path)
+    end
   end
+  
+  private
+  #Strong Parameter
+  def food_params
+    params.require(:food).permit(:name, :hiragana, :katakana, :kanzi, :other1, :other2, :other3 )
+  end
+  
 end
