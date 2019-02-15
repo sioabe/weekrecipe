8fe50bc4df6616a295ead0a6bebc20a8113ade1f
sio <m40x3.1wa121@gmail.com>
2019-02-15 09:18:09 +0000

    マイグレーションファイルのカラム名を一部変更。

--- db/migrate/20190131004458_add_update_date_to_recipe.rb
+++ db/migrate/20190131004458_add_update_date_to_recipe.rb
@@ -1,5 +1,5 @@
 class AddUpdateDateToRecipe < ActiveRecord::Migration[5.0]
   def change
-    add_column :recipes, :update_date, :DateTime
+    add_column :recipes, :update_date, :datetime
   end
 end
